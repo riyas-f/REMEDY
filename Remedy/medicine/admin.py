bad94a7b63a209466c1ac52f5b93d9f2873c1ccd
Nidheesh P <nidhinmanu07@gmail.com>
2022-12-18 00:19:37 +0530

    Nov 18

--- Remedy/medicine/admin.py
+++ Remedy/medicine/admin.py
@@ -3,3 +3,4 @@ from .models import *
 # Register your models here.
 
 admin.site.register(MedicineRecord)
+admin.site.register(MedicineName)
--- Remedy/medicine/models.py
+++ Remedy/medicine/models.py
@@ -2,16 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-class MedicineRecord(models.Model):
+class MedicineName(models.Model):
     medicine_name=models.CharField(max_length=100)
 
     class Meta:
         verbose_name_plural = "Medicine Name"
-        ordering = ['medicine_name']
 
     def __str__(self):
         return self.medicine_name
 
+class MedicineRecord(models.Model):
+    medicine_name=models.ForeignKey(MedicineName,on_delete=models.PROTECT,related_name='+')
     medicine_details=models.TextField()
 
     class Meta:
@@ -19,3 +20,5 @@ class MedicineRecord(models.Model):
 
     def __str__(self):
         return self.medicine_details
+
+

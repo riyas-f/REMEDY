dc18afc4e12dfde5a3b52ff1d850b30a338e9d45
Nidheesh P <nidhinmanu07@gmail.com>
2022-12-18 05:14:45 +0530

    Nov 18

--- Remedy/accounts/views.py
+++ Remedy/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.models import auth
+from django.contrib.auth.models import auth,User
 
 # Create your views here.
 
@@ -25,17 +25,30 @@ def logout(request):
     return redirect('/')
 
 def register(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = auth.authenticate(username=username,password=password)
-        if user is not None:
-            auth.login(request, user)
-            return redirect('/')
+    if request.method=="POST":
+        first_name = request.POST['fname']
+        last_name = request.POST['lname']
+        username = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        email = request.POST['email']
+        if password1==password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,"username already taken")
+                return redirect('/register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,"email already taken")
+                return redirect('/register')
+            else:
+                user = User.objects.create_user(username=username, password=password1, first_name=first_name,
+                                            last_name=last_name, email=email)
+                user.save()
+                messages.info(request,"Account Created")
+                print("user created")
         else:
-            messages.info(request,'Invalid Credentials')
-            return redirect('/login/')
-
+            print("password not matched")
+            return redirect('/register')
+        return redirect('/')
     else:
-        return render(request, 'login.html')
+
+        return render(request,'register.html')
--- Remedy/medicine/models.py
+++ Remedy/medicine/models.py
@@ -2,9 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-class MedicineName(models.Model):
-    medicine_name=models.CharField(max_length=100)
+class MedicineType(models.Model):
+    type = models.CharField(max_length=100)
 
+class MedicineTime(models.Model):
+    time = models.TimeField()
+
+    def __str__(self):
+        return str(self.time)
+
+class MedicineName(models.Model):
+    medicine_name=models.CharField(max_length=100)    
     class Meta:
         verbose_name_plural = "Medicine Name"
 
@@ -14,11 +22,17 @@ class MedicineName(models.Model):
 class MedicineRecord(models.Model):
     medicine_name=models.ForeignKey(MedicineName,on_delete=models.PROTECT,related_name='+')
     medicine_details=models.TextField()
-
     class Meta:
         verbose_name_plural = "Medicine Details"
 
     def __str__(self):
         return self.medicine_details
 
+class SavedMedicines(models.Model):
+    medicine_name=models.ForeignKey(MedicineName,on_delete=models.PROTECT,related_name='+')
+    time=models.ManyToManyField(MedicineTime)
+    dosage = models.IntegerField()
+    timeof = models.BooleanField()
 
+    def __str__(self):
+        return self.medicine_name
